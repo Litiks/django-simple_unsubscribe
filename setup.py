5f4844e05e8b71507760fec994a4e40b5a59259b
Aaron Mader <a.mader@gmail.com>
2015-11-10 16:26:38 -0500

    final touchups

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
   name='django-simple_unsubscribe',
-  description='A very simple unsubscribe management addon for Django',
+  description='A very simple (email) unsubscribe management addon for Django',
   packages=['unsubscribe'],
 )
