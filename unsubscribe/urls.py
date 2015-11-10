0dc726081426eed0b77908035d6ed6d575038533
Aaron Mader <a.mader@gmail.com>
2015-11-10 16:38:12 -0500

    adjusting requiremetns

--- unsubscribe/urls.py
+++ unsubscribe/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('unsubscribe.views',
     url(r'^(?P<token>.+)/$', 'unsubscribe', name='unsubscribe'),
