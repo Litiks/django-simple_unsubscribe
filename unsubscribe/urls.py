00d3e1c55294c4eee6df2127cdf90f742a5103da
Aaron Mader <a.mader@gmail.com>
2015-11-11 14:45:28 -0500

    don't be greedy

--- unsubscribe/urls.py
+++ unsubscribe/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('unsubscribe.views',
-    url(r'^(?P<token>.+)/$', 'unsubscribe', name='unsubscribe'),
-    url(r'^(?P<token>.+)/complete/$', 'unsubscribe_complete', name='unsubscribe_complete'),
-    url(r'^restore/(?P<token>.+)/$', 'resubscribe', name='resubscribe'),
-    url(r'^restore/(?P<token>.+)/complete/$', 'resubscribe_complete', name='resubscribe_complete'),
+    url(r'^(?P<token>.+?)/$', 'unsubscribe', name='unsubscribe'),
+    url(r'^(?P<token>.+?)/complete/$', 'unsubscribe_complete', name='unsubscribe_complete'),
+    url(r'^restore/(?P<token>.+?)/$', 'resubscribe', name='resubscribe'),
+    url(r'^restore/(?P<token>.+?)/complete/$', 'resubscribe_complete', name='resubscribe_complete'),
 )
